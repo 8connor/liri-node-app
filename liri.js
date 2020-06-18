9ac6515a84ad80abe596f907a61dcd53633a1b7d
The-Napster <connorh16@gmail.com>
2020-06-18 02:23:58 -0400

    123

--- liri.js
+++ liri.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
-require("node-spotify-api");
+// require("node-spotify-api");
+// require("axios");
+// require("omdb");
+// require("bit_js");
+// require("moment");
 
 var keys = require("./keys.js");
+
 var spotify = new Spotify(keys.spotify);
 
+// var artist = process.argv[3].trim()
 
-var axios = require("axios");
-var omdb = require("omdb");
-var bit = require("bit_js");
-var moment = require("moment");
-var dotenv = require("dotenv");
+// var queryURL = "https://rest.bandsintown.com/artists/" 
+// + artist 
+// + "/events?app_id=codingbootcamp"
+
+// axios.get(queryURL).then(function (response) {
+//     console.log(response)
+// })
+
+// console.log("123")
